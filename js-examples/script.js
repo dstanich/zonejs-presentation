9794f3c8148d48500a22d20c7c0135f814a52d4e
David Stanich <dstanich@gmail.com>
2017-10-29 05:30:47 -0500

    Commit

--- js-examples/script.js
+++ js-examples/script.js
@@ -1,3 +1,11 @@
 function init() {
+  console.log('init()', Zone.current);
+  simpleSetTimeout();
+}
 
+function simpleSetTimeout() {
+  setTimeout(() => {
+    console.log('simpleSetTimeout()', Zone.current);
+    debugger;
+  }, 1000);
 }
