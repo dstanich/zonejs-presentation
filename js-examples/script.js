ee085ff5af34a8b382885f72bc41535b360410ad
David Stanich <dstanich@gmail.com>
2017-10-29 09:59:04 -0500

    Update examples

--- js-examples/script.js
+++ js-examples/script.js
@@ -8,12 +8,14 @@ let timingSpec = {
     }
   },
   onInvoke: (parentZoneDelegate, currentZone, targetZone, delegate, applyThis, applyArgs, source) => {
+    // run() was called, start the timer
     targetZone.get('timer').start = new Date();
     console.log('run() is called', targetZone.get('timer').start.getTime());
     parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
   },
   onHasTask: (parentZoneDelegate, currentZone, targetZone, hasTaskState) => {
-    console.log(hasTaskState);
+    // Zone queue was changed, if it is now empty then stop the timer
+    console.log('hasTaskState: ', hasTaskState);
     if (!hasTaskState.microTask && !hasTaskState.macroTask && !hasTaskState.eventTask) {
       targetZone.get('timer').end = new Date();
       console.log('Total time: ', targetZone.get('timer').end - targetZone.get('timer').start);
@@ -21,24 +23,65 @@ let timingSpec = {
   }
 };
 
-function init() {
-  console.log('init()', Zone.current);
-  let start;
+function zoneTimerExample() {
+  console.log('zoneTimerExample()', Zone.current);
 
-  // Fork
-  const newFork = Zone.current.fork(timingSpec);
-  console.log('my-new-fork', newFork);
+  // Create fork based on timer spec
+  const timerFork = Zone.current.fork(timingSpec);
+  console.log('timer-fork', timerFork);
 
-  newFork.run(() => {
-    for (let i = 0; i < 10; i++) {
-      simpleSetTimeout(i);
+  // Kickoff code which will schedule MacroTasks via setTimeout
+  timerFork.run(() => {
+    for (let i = 1; i <= 10; i++) {
+      setTimeout(() => {
+        console.log('timeout ' + i + ' was run');
+      }, Math.random() * 1000 * i);
     }
   });
 }
 
-function simpleSetTimeout(i) {
+function multipleForksExample() {
+  console.log('multipleForksExample()', Zone.current);
+
+  // First fork; sets up onFork hook
+  const newFork = Zone.current.fork({
+    name: 'my-new-fork',
+    onFork: (parentZoneDelegate, currentZone, targetZone, zoneSpec) => {
+      console.log('onFork() hook -- forking new zone with spec: ', zoneSpec);
+      return parentZoneDelegate.fork(targetZone, zoneSpec);
+    }
+  });
+  console.log('my-new-fork', newFork);
+
+  // Fork again, based on first fork.  Will trigger onFork in first fork.
+  console.log('Creating a second fork...');
+  const secondFork = newFork.fork({name: 'second-fork'});
+
+  // run() using the second fork
+  secondFork.run(() => {
+    console.log('Log within second-fork run', Zone.current);
+  })
+}
+
+function setTimeoutExample() {
+  console.log('setTimeoutExample()', Zone.current);
+
+  // Fork
+  const newFork = Zone.current.fork({
+    name: 'my-new-fork'
+  });
+  console.log('my-new-fork', newFork);
+  newFork.run(() => {
+    simpleSetTimeout();
+  });
+
+  // Call function with root zone
+  simpleSetTimeout();
+}
+
+function simpleSetTimeout() {
   setTimeout(() => {
-    console.log('simpleSetTimeout()', i);
+    console.log('simpleSetTimeout()', Zone.current);
     // debugger;
-  }, Math.random() * 5000);
+  }, 1000);
 }
