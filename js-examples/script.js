b8ddee4f1b2ad0692295ceb461bbc2e5fc56fe48
David Stanich <dstanich@gmail.com>
2017-10-29 09:44:46 -0500

    Update code

--- js-examples/script.js
+++ js-examples/script.js
@@ -1,22 +1,40 @@
-function init() {
-  console.log('init()', Zone.current);
+function multipleForksExample() {
+  console.log('multipleForks()', Zone.current);
 
-  // Fork
+  // First fork; sets up onFork hook
   const newFork = Zone.current.fork({
     name: 'my-new-fork',
     onFork: (parentZoneDelegate, currentZone, targetZone, zoneSpec) => {
-      console.log('Forking new zone', zoneSpec);
+      console.log('onFork() hook -- forking new zone with spec: ', zoneSpec);
       return parentZoneDelegate.fork(targetZone, zoneSpec);
     }
   });
   console.log('my-new-fork', newFork);
 
-  // Fork again, based on first fork
+  // Fork again, based on first fork.  Will trigger onFork in first fork.
   console.log('Creating a second fork...');
   const secondFork = newFork.fork({name: 'second-fork'});
+
+  // run() using the second fork
   secondFork.run(() => {
     console.log('Log within second-fork run', Zone.current);
   })
+}
+
+function setTimeoutExample() {
+  console.log('setTimeoutExample()', Zone.current);
+
+  // Fork
+  const newFork = Zone.current.fork({
+    name: 'my-new-fork'
+  });
+  console.log('my-new-fork', newFork);
+  newFork.run(() => {
+    simpleSetTimeout();
+  });
+
+  // Call function with root zone
+  simpleSetTimeout();
 }
 
 function simpleSetTimeout() {
