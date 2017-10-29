5e826dece18682d49cbb0930393396dfd0ec3790
David Stanich <dstanich@gmail.com>
2017-10-29 05:54:27 -0500

    Commit

--- js-examples/script.js
+++ js-examples/script.js
@@ -3,15 +3,20 @@ function init() {
 
   // Fork
   const newFork = Zone.current.fork({
-    name: 'my-new-fork'
+    name: 'my-new-fork',
+    onFork: (parentZoneDelegate, currentZone, targetZone, zoneSpec) => {
+      console.log('Forking new zone', zoneSpec);
+      return parentZoneDelegate.fork(targetZone, zoneSpec);
+    }
   });
   console.log('my-new-fork', newFork);
-  newFork.run(() => {
-    simpleSetTimeout();
-  });
 
-  // Call function with root zone
-  simpleSetTimeout();
+  // Fork again, based on first fork
+  console.log('Creating a second fork...');
+  const secondFork = newFork.fork({name: 'second-fork'});
+  secondFork.run(() => {
+    console.log('Log within second-fork run', Zone.current);
+  })
 }
 
 function simpleSetTimeout() {
