66f78b31c8662b2332ca5b67f2a88d2f96ae7887
David Stanich <dstanich@gmail.com>
2017-10-31 05:32:32 -0500

    Add code

--- ngx-examples/src/app/app.component.ts
+++ ngx-examples/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'zone.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(
+    private zone: NgZone,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    console.log('Current zone', Zone.current);
+
+    this.zone.onUnstable.subscribe(() => {
+      console.log('Unstable');
+    });
+
+    this.zone.onMicrotaskEmpty.subscribe(() => {
+      console.log('Microtask empty -- change detection is run');
+    });
+
+    this.zone.onStable.subscribe(() => {
+      console.log('Stable');
+    });
+
+    this.http.get('https://api.spacexdata.com/v1/launches').subscribe(response => {
+      console.log(response);
+    });
+  }
 }
